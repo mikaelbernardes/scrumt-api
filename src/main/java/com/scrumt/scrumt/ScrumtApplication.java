49f0cbc994a188518849d5f2551fcc634cbb327d
Mikael <bernardesmikael@outlook.com>
2024-11-30 15:50:52 -0300

    start scrum api project

---
+++ src/main/java/com/scrumt/scrumt/ScrumtApplication.java
@@ -0,0 +1,13 @@
+package com.scrumt.scrumt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrumt/scrumt/ScrumtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrumt.scrumt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
